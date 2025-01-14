c025f8f57539fa54c47bb294414f3bd03b74e6f9
Fabiski <fabrice.renault@edu.devinci.fr>
2025-01-14 14:02:51 +0100

    added setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+name='{GameOfLife}',
+version='1.0',
+author='{FabriceRenault}',
+license='MIT',
+long_description=open('README.md').read(),
+)
